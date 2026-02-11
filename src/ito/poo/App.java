88bf9777c8538da9633776439f769f0b6e5d105c
Oswaldo Sosa Pérez <sosa.perez.oswaldo@gmail.com>
2026-02-10 20:12:47 -0600

    primer commit con los codigos fuentes sin copilar

---
+++ src/ito/poo/App.java
@@ -0,0 +1,15 @@
+package ito.poo;
+
+public class App {
+    static Mascota mascota;
+
+    static void run(){
+        mascota= new Mascota("satur", 2, "perro", "chihuahua");
+        System.out.println(mascota);
+        mascota.cumpleAños();
+        System.out.println("Despues de cumplir año"+mascota);
+
+    }
+    public static void main (String[] args){run();}
+}
+
